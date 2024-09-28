f41f688fd8196d4234267ac1e4e1bcbe5e509309
SlimeNull <slimenull@outlook.com>
2024-09-29 06:20:30 +0800

    添加友链

--- src/config.ts
+++ src/config.ts
@@ -39,6 +39,10 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Archive,
     LinkPreset.About,
     {
+      name: '友链',
+      url: '/friends/'
+    },
+    {
       name: 'GitHub',
       url: 'https://github.com/SlimeNull',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
---
+++ src/content/spec/friends.md
@@ -0,0 +1,6 @@
+诺尔有很多朋友~
+
+- [ilyfairy](https://blog.233.pink/)
+  擅长使用 .NET 高阶魔法的魔法师
+- [TwilightLemon](https://blog.twlmgatito.cn/)
+  年轻又有才华的优雅设计师与程序员
